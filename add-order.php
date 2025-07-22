102a55a72d8b7b0117c00a97c6fea73d731b0f95
Ash-8448 <ashleycandelaria0@gmail.com>
2025-07-23 00:08:01 +0800

    modified:   add-order.php

--- add-order.php
+++ add-order.php
@@ -24,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Add Order</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="order_style.css">
 </head>
 <body class="bg-light">
 
