9304e1a87ff8ceb198b628b2cc8d19d1825895c1
Ash-8448 <ashleycandelaria0@gmail.com>
2025-07-24 16:54:06 +0800

    modified:   admin/order-table.sql

--- admin/order-table.sql
+++ admin/order-table.sql
@@ -11,4 +11,10 @@ CREATE TABLE `orders` (
    PRIMARY KEY (`order_id`)
  ) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
- SELECT * FROM orders;
+SELECT * FROM orders;
+
+INSERT INTO orders (customer_id, product_id, item_quantity, total_amount, date_created, status, payment, discount)
+VALUES 
+('1001', '1001', '15', 15000.00, '2025-07-16', 'Processing' 'Credit Card', 0.00),
+('1002', '1001', '12', 12000.00, '2025-07-16', 'Shipped' 'Cash', 0.00),
+('1003', '1002', '5', 1000.00, '2025-07-16', 'Delivered' 'GCash', 0.00);
