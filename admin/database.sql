09c242f19432289dbf3dbcaa4e936fdf361f604d
Lee Ramirez <leeramirez015@gmail.com>
2025-07-25 19:09:37 +0800

    removed total_order and total_spent from customers table

--- admin/database.sql
+++ admin/database.sql
@@ -10,8 +10,6 @@ USE erp_db;
    `e-mail` varchar(45) NOT NULL,
    `contact_no` bigint(10) NOT NULL,
    `credit_card_no` int(15) DEFAULT NULL,
-   `total_orders` int(11) NOT NULL,
-   `total_spent` decimal(2,0) NOT NULL,
    `date_created` datetime NOT NULL DEFAULT current_timestamp(),
    PRIMARY KEY (`customer_id`)
  ) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
