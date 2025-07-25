50fd4fb0e55a9ac09715854b18c0fdef2fc64947
Lee Ramirez <leeramirez015@gmail.com>
2025-07-25 20:52:06 +0800

    updated dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -4,18 +4,8 @@ function textColor($percentage) {
     return $percentage > 0 ? 'positive' : 'negative';
 }
 
-function getPercentage($prev_month, $current_month) {
-    if ($prev_month > 0) {
-        $difference = 100 * (($current_month - $prev_month) / $prev_month);
-        $difference = number_format($difference, 2);
-        if ($difference >= 0) {
-            return "+$difference";
-        } elseif ($difference < 0) {
-            return "$difference";
-        }
-    } else {
-        return "0";
-    }
+function numberSign($percentage) {
+    return $percentage > 0 ? "+" : "";
 }
 
 ?>
@@ -32,18 +22,26 @@ function getPercentage($prev_month, $current_month) {
         <div class="container-fluid">
 
             <!-- 1st card row -->
+            <?php
+            $total_revenue = $conn->query("SELECT SUM(a.total_payment) - SUM(a.quantity * b.cost_price) as total FROM invoices a
+                INNER JOIN products b ON a.product_id = b.product_id;")->fetch_assoc()['total'];
+            $revenue_percentage = $conn->query("SELECT 100 * ((SELECT SUM(p.cost_price * i.quantity) FROM invoices i
+                INNER JOIN products p ON p.product_id = i.product_id
+                WHERE MONTH(i.date_placed) = MONTH(CURRENT_DATE() - INTERVAL 1 MONTH))
+                - (SELECT SUM(p.cost_price * i.quantity) FROM invoices i
+                INNER JOIN products p ON p.product_id = i.product_id
+                WHERE MONTH(i.date_placed) = MONTH(CURRENT_DATE())))
+                / (SELECT SUM(p.cost_price * i.quantity) FROM invoices i
+                INNER JOIN products p ON p.product_id = i.product_id
+                WHERE MONTH(i.date_placed) = MONTH(CURRENT_DATE() - INTERVAL 1 MONTH)) AS percentage;")->fetch_assoc()['percentage'];
+            ?>
             <div class="row">
-                <?php
-                $total_customers = $conn->query("SELECT COUNT(customer_id) AS total_customers FROM customers;")->fetch_assoc()['total_customers'];
-                $new_customers = $conn->query("SELECT COUNT(customer_id) AS new_customers FROM customers
-                WHERE MONTH(date_created) = MONTH(CURRENT_DATE());")->fetch_assoc()['new_customers'];
-                ?>
                 <div class="col-6">
                     <div class="card" style="border: 1px solid #dee2e6; border-radius: 10px;">
                         <h5 class="text-secondary">Total Revenue</h5>
-                        <h2 class="text-dark">₱ 10</h2>
-                        <div class="card-text">
-                            <caption class="card-caption">100</caption>
+                        <h2 class="text-dark">₱ <?= $total_revenue ?></h2>
+                        <div class="card-text <?= textColor($revenue_percentage) ?>">
+                            <caption class="card-caption"><?= numberSign($revenue_percentage).number_format($revenue_percentage, 0) ?>% than last month</caption>
                         </div>
                     </div>
                 </div>
@@ -59,12 +57,17 @@ function getPercentage($prev_month, $current_month) {
             </div>
 
             <!-- 2nd card row -->
+            <?php
+            $total_customers = $conn->query("SELECT COUNT(customer_id) AS total_customers FROM customers;")->fetch_assoc()['total_customers'];
+            $new_customers = $conn->query("SELECT COUNT(customer_id) AS new_customers FROM customers
+                    WHERE MONTH(date_created) = MONTH(CURRENT_DATE());")->fetch_assoc()['new_customers'];
+            ?>
             <div class="row">
                 <div class="col-6">
                     <div class="card" style="border: 1px solid #dee2e6; border-radius: 10px;">
                         <h5 class="text-secondary">Customers</h5>
                         <h2 class="text-dark"><?= $total_customers ?></h2>
-                        <div class="card-text">
+                        <div class="card-text"" style="color: #27AC49">
                             <caption class="card-caption"><?= $new_customers ?> new this month</caption>
                         </div>
                     </div>
@@ -72,16 +75,16 @@ function getPercentage($prev_month, $current_month) {
 
                 <?php
                 $products_sold = $conn->query("SELECT COUNT(*) AS total_sold FROM invoices")->fetch_assoc()['total_sold'];
-                $current_month_sales = $conn->query("SELECT COUNT(*) AS current_sales FROM invoices WHERE month(date_placed) = MONTH(CURRENT_DATE());")->fetch_assoc()['current_sales'];
-                $prev_month_sales =  $conn->query("SELECT COUNT(*) AS prev_sales FROM invoices WHERE month(date_placed) = MONTH(CURRENT_DATE() - INTERVAL 1 MONTH);")->fetch_assoc()['prev_sales'];
-                $percentage = getPercentage($prev_month_sales, $current_month_sales);
+                $products_percentage = $conn->query("SELECT 100 * ((SELECT COUNT(*) FROM invoices WHERE MONTH(date_placed) = MONTH(CURRENT_DATE()))
+                    - (SELECT COUNT(*) FROM invoices WHERE MONTH(date_placed) = MONTH(CURRENT_DATE() - INTERVAL 1 MONTH)))
+                    / (SELECT COUNT(*) FROM invoices WHERE MONTH(date_placed) = MONTH(CURRENT_DATE() - INTERVAL 1 MONTH)) as percentage;")->fetch_assoc()['percentage'];
                 ?>
                 <div class="col-6">
                     <div class="card" style="border: 1px solid #dee2e6; border-radius: 10px;">
                         <h5 class="text-secondary">Products Sold</h5>
                         <h2 class="text-dark"><?= $products_sold ?></h2>
-                        <div class="card-text <?= textColor($percentage) ?>">
-                            <caption class="card-caption"><?= $percentage ?>% than previous month</caption>
+                        <div class="card-text <?= textColor($products_percentage) ?>">
+                            <caption class="card-caption"><?= numberSign($products_percentage).number_format($products_percentage, 0) ?>% than previous month</caption>
                         </div>
                     </div>
                 </div>
