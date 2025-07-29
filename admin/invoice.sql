6ea4438303c89df1ac16dc2b4910df11b43b1291
rviiirae <keithmontenegro14@gmail.com>
2025-07-30 00:10:29 +0800

    Update invoice.sql

--- admin/invoice.sql
+++ admin/invoice.sql
@@ -1,12 +1,14 @@
  CREATE TABLE `invoices` (
-   `order_id` int(11) NOT NULL,
-   `customer_id` int(11) NOT NULL,
-   `product_id` int(11) NOT NULL,
-   `quantity` varchar(45) NOT NULL,
-   `discount_applied` decimal(2,2) NOT NULL,
-   `total_payment` decimal(10,2) NOT NULL DEFAULT 0.00,
-   `status` varchar(15) NOT NULL,
-   `date_placed` datetime NOT NULL,
-   `date_completed` datetime NOT NULL DEFAULT current_timestamp(),
-   PRIMARY KEY (`order_id`)
- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+    `order_id` INT(11) NOT NULL,
+    `customer_id` INT(11) NOT NULL,
+    `product_id` INT(11) NOT NULL,
+    `quantity` VARCHAR(45) NOT NULL,
+    `discount_applied` DECIMAL(5 , 2) NOT NULL,
+    `total_payment` DECIMAL(10 , 2 ) NOT NULL DEFAULT 0.00,
+    `status` VARCHAR(15) NOT NULL,
+    `payment_method` VARCHAR(50) NOT NULL,
+    `date_placed` DATETIME NOT NULL,
+    `date_completed` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP (),
+    PRIMARY KEY (`order_id`)
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8MB4 COLLATE = UTF8MB4_GENERAL_CI;
+ 
